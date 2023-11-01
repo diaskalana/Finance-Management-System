9179f3f83ef5782c1f3d740bfd42a2aca39b92d1
Kalana Dias <kalanasocial@gmail.com>
2023-11-01 12:36:52 +0530

    some changes

--- dbConnect.js
+++ dbConnect.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const MongoDB_URI = ''  //Enter your MongoDB URI here
 
-const connect = mongoose.connect('mongodb+srv://kalanadias:Kalana123@cluster0.ewbh7py.mongodb.net/finance_management', { useNewUrlParser: true, useUnifiedTopology: true })
+const connect = mongoose.connect(MongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const connection = mongoose.connection
 
