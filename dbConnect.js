5c43432806f814c09b9a6f752551b54099387a54
diaskalana <74653324+diaskalana@users.noreply.github.com>
2023-12-23 22:17:13 +0530

    fix bugs

--- dbConnect.js
+++ dbConnect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const MongoDB_URI = ''  //Enter your MongoDB URI here
+const MongoDB_URI = 'mongodb+srv://kalanadias:Kalana123@cluster0.ewbh7py.mongodb.net/finance_management'  //Enter your MongoDB URI here
 
 const connect = mongoose.connect(MongoDB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
